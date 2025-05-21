4199edca8eac5265ee98c510bc602bfb38706397
Cole Leavitt <75138914+coleleavitt@users.noreply.github.com>
2025-05-20 22:20:11 -0700

    Update debug_run.sh
    sowwy

--- debug_run.sh
+++ debug_run.sh
@@ -3,4 +3,4 @@ for pid in $(pgrep -f spotify); do
     kill -9 $pid
 done
 
-LD_PRELOAD=/home/cole/projects/spotify-adblock/target/release/libspotifyadblock.so spotify --enable-features=useozoneplatform --ozone-platform=wayland
+LD_PRELOAD=./target/release/libspotifyadblock.so spotify --enable-features=useozoneplatform --ozone-platform=wayland
